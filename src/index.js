f00d0b00798b4b5a249cf82f8f0df05d6cd8c7b9
Arian Xhumkar <arianxhumkar@hotmail.com>
2025-01-09 11:17:00 +0100

    Fixed npm install version compatibility of dependencies

--- src/index.js
+++ src/index.js
@@ -3,12 +3,13 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import './i18n';
 //import UnderConstruction from './components/UnderConstruction';
-import App from './App';
+//import App from './App';
+import UnderConstruction from './components/UnderConstruction';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <UnderConstruction />
   </React.StrictMode>
 );
 
