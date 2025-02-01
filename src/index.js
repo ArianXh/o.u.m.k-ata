efbf2394241c2935a26184863e1ac097f7f1e3b2
Arian Xhumkar <arianxhumkar@hotmail.com>
2025-02-01 15:06:52 +0100

    Test deployment

--- src/index.js
+++ src/index.js
@@ -2,13 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import './i18n';
-//import App from './App';
-import UnderConstruction from './components/UnderConstruction';
+import App from './App';
+// import UnderConstruction from './components/UnderConstruction';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <UnderConstruction />
+    <App />
   </React.StrictMode>
 );
 
